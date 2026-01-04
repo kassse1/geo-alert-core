90b305116e256cd53bf4f6a143769a53b85f4920
kassse1 <124445863+kassse1@users.noreply.github.com>
2026-01-04 15:26:16 +0500

    Update location_check.go
    improvement for code readability

--- internal/domain/location_check.go
+++ internal/domain/location_check.go
@@ -3,9 +3,15 @@ package domain
 import "time"
 
 type LocationCheck struct {
-	ID        int64
-	UserID    string
-	Lat       float64
-	Lon       float64
-	CheckedAt time.Time
+	ID           int64
+	UserID       string
+	Lat          float64
+	Lon          float64
+
+	IncidentIDs  []int64    // какие инциденты были найдены
+	HasDanger    bool       // был ли риск
+	DistanceM    int        // минимальная дистанция до зоны
+
+	CheckedAt    time.Time
 }
+
