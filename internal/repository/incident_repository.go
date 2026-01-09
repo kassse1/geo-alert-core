3e7f392ca70a7c7e2b0fafe6bb54c5ba09fe4b27
kassse1 <124445863+kassse1@users.noreply.github.com>
2026-01-09 22:14:00 +0500

    Update incident_repository.go

--- internal/repository/incident_repository.go
+++ internal/repository/incident_repository.go
@@ -2,7 +2,7 @@ package repository
 
 import "github.com/kassse1/geo-alert-core/internal/domain"
 
-// IncidentRepository описывает работу с инцидентами в хранилище
+
 type IncidentRepository interface {
 	Create(incident *domain.Incident) error
 	GetByID(id int64) (*domain.Incident, error)
